046b3e5800a698f33c7c5b580bd255981e98a047
Yoon Nandar Kyaw <YoonNandarKyaw30>
2024-01-14 00:35:44 +0630

    initial commit

---
+++ src/main/java/org/example/onlinebookshop/OnlineBookshopApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/onlinebookshop/OnlineBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
